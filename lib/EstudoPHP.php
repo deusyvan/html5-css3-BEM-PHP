90d72cf6e6ff93a70074d1379be4bad3d1ab8477
deusyvan <deusyvan@gmail.com>
2018-02-16 11:02:09 -0200

    Referência de variável

--- lib/EstudoPHP.php
+++ lib/EstudoPHP.php
@@ -1,12 +1,12 @@
 <?php
 
-//Atribuição de variável
+//Referência de variável
 
 $a = 5;
-$b = $a;
+$b = &$a;
 $b = 10;
 
-echo $a; //resultado = 5
+echo $a; //resultado = 10
 echo "\n***************\n";
 echo $b; //resultado = 10
 
