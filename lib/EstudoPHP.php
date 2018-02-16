d1b7cf405ef30ea415a3d6df8051d31bced794c9
deusyvan <deusyvan@gmail.com>
2018-02-16 19:04:07 -0200

    Tipo String

--- lib/EstudoPHP.php
+++ lib/EstudoPHP.php
@@ -1,18 +1,9 @@
 <?php
 
-//Tipo Numérico
-$decimal = 1234;
+//Tipo String
+$variavel = 'Isto é um teste';
 
-$negativo = -123;
-
-$octal = 0123;
-
-$hexadecimal = 0x1A;
-
-$pontoFlutuante = 1.234;
-
-$notacao_cientifica = 4e23;
-
+$variavel = "Isto é um teste";
 
 
 echo "\n***************\n";
