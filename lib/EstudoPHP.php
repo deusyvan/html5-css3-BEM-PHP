93b26961176216ef29a7f18b9fae8540ab329a60
deusyvan <deusyvan@gmail.com>
2018-02-17 16:29:04 -0200

    Operadores Relacionais - utilização errada do op de atribuição =

--- lib/EstudoPHP.php
+++ lib/EstudoPHP.php
@@ -1,16 +1,31 @@
 <?php
 
-//Conversão de tipos
+//Operadores Relacionais
 
-$a = '10'; 
-echo $a + 5;
-
+//Utilização errada do operador de atribuição "="
+if ($a = 5){
+    echo 'essa operação atribui 5 à variável $a e retorna verdadeiro';
+}
 echo "\n***************\n";
 
 //Resultados:
 /*
  
- 15
+ essa operação atribui 5 à variável $a e retorna verdadeiro
   
  */
+
+/* 
+  
+ ==
+ ===
+ != OU <>
+ <
+ >
+ <=
+ >=
+ 
+ 
+ */
+
 
