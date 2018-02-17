cc4c7193bdbcfd681f54ce713abc5113672049c4
deusyvan <deusyvan@gmail.com>
2018-02-16 23:01:20 -0200

    Operadores Aritméticos : usado para realização de cálculos matemáticos

--- lib/EstudoPHP.php
+++ lib/EstudoPHP.php
@@ -1,21 +1,33 @@
 <?php
 
-//Operadores Atribuição
+//Operadores Aritméticos : usado para realização de cálculos matemáticos
 
-$var += 5; //soma 5 em $var
-$var -= 5; //subtrai 5 em $var;
-$var *= 5; //multiplicar $var por 5
-$var /= 5; //divide $var por 5
+/* 
+ 
+ 
+ +
+ -
+ *
+ /
+ % 
+ 
 
-++$a; // Pré-incremento incrementa $a em um e, então, retorna $a
-$a++; // Pós-incremento Retorna $a e, então, incrementa $a em um
---$a; // Pré-decremento incrementa $a em um e, então, retorna $a
-$a--; // Pós-decremento Retorna $a e, então, decrementa $a em um
+ */
+
+
+$a = 2; 
+$b = 4; 
+echo $a+3*4+5*$b;
 
 echo "\n***************\n";
+
+echo ($a+3)*4+(5*$b);
+
 //Resultados:
 /*
+ 34
  
+ 40
  
  */
 
