a26d8fc8e7e91a17066d17779e8899d750f1770b
deusyvan <deusyvan@gmail.com>
2018-02-16 22:52:43 -0200

    Operadores Atribuição

--- lib/EstudoPHP.php
+++ lib/EstudoPHP.php
@@ -1,18 +1,21 @@
 <?php
 
-//Constantes
+//Operadores Atribuição
 
-//MAXIMO_CLIENTES Para definir usa a função define()
+$var += 5; //soma 5 em $var
+$var -= 5; //subtrai 5 em $var;
+$var *= 5; //multiplicar $var por 5
+$var /= 5; //divide $var por 5
 
-define("MAXIMO_CLIENTES", 100);
-
-echo MAXIMO_CLIENTES;
+++$a; // Pré-incremento incrementa $a em um e, então, retorna $a
+$a++; // Pós-incremento Retorna $a e, então, incrementa $a em um
+--$a; // Pré-decremento incrementa $a em um e, então, retorna $a
+$a--; // Pós-decremento Retorna $a e, então, decrementa $a em um
 
 echo "\n***************\n";
 //Resultados:
 /*
  
-100
-  
+ 
  */
 
