c0b3288dc8284b49506110abc567618cdfc6fa94
deusyvan <deusyvan@gmail.com>
2018-02-18 10:49:41 -0300

    PHP - Operadores Relacionais: uso do === e !==

--- classe/EstudoPHP.php
+++ classe/EstudoPHP.php
@@ -1,21 +1,23 @@
 <?php
 
 //Operadores Relacionais
-// uso do == e !=
+// uso do == e != comparando os tipos de dados
 
 $a = 1234;
 $b = '1234';
 
-if ($a == $b){
-    echo '$a e $b são iguais';
-} else if ($a != $b){
-    echo '$a e $b são diferentes';
+if ($a === $b){
+    echo '$a e $b são iguais e do mesmo tipo';
+} 
+
+if ($a !== $b){
+    echo '$a e $b são de tipos diferentes';
 }
 echo "\n***************\n";
 
 //Resultados:
 /*
  
-$a e $b são iguais
- 
+$a e $b são de tipos diferentes
+
  */
