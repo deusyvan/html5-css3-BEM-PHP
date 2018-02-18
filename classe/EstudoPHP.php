825b50665bd80103a4f271cc8cdc020b7486a5bb
deusyvan <deusyvan@gmail.com>
2018-02-18 13:05:15 -0300

    PHP - Estruturas de controle: SWITCH

--- classe/EstudoPHP.php
+++ classe/EstudoPHP.php
@@ -1,16 +1,38 @@
 <?php
 
 //Estruturas de controle
-// FOR
-for ($i = 1; $i <= 10; $i++){
-    print $i;
+// SWITCH
+$i = 1;
+if($i == 0){
+    print "i eh igual a zero";
+} elseif ($i == 1){
+    print "i eh igual a um";
+} elseif ($i == 2){
+    print "i eh igual a dois";
+} else {
+    print "i nao eh igual a 0, 1 ou 2";
 }
 
 echo "\n***************\n";
+$i = 3;
 
+switch ($i){
+    case 0:
+        print "i eh igual a 0";
+        break;
+    case 1:
+        print "i eh igual a 1";
+        break;
+    case 2:
+        print "i eh igual a 2";
+        break;
+    default:
+        print "i nao eh igual a 0, 1 ou 2";
+}
 //Resultados:
 /*
     
-12345678910
+i eh igual a um
     
+i nao eh igual a 0, 1 ou 2
  */
