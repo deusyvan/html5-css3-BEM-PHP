5bc5fd7e6e99761ea07c861dcae6d9f594f80df5
deusyvan <deusyvan@gmail.com>
2018-02-18 13:10:44 -0300

    PHP - Estruturas de controle: FOREACH para arrays ou matrizes

--- classe/EstudoPHP.php
+++ classe/EstudoPHP.php
@@ -1,38 +1,18 @@
 <?php
 
 //Estruturas de controle
-// SWITCH
-$i = 1;
-if($i == 0){
-    print "i eh igual a zero";
-} elseif ($i == 1){
-    print "i eh igual a um";
-} elseif ($i == 2){
-    print "i eh igual a dois";
-} else {
-    print "i nao eh igual a 0, 1 ou 2";
-}
+// FOREACH para arrays ou matrizes
 
+$fruta = array("maca", "laranja", "pera" , "banana");
+
+foreach ($fruta as $valor){
+    print "$valor - ";
+}
 echo "\n***************\n";
-$i = 3;
 
-switch ($i){
-    case 0:
-        print "i eh igual a 0";
-        break;
-    case 1:
-        print "i eh igual a 1";
-        break;
-    case 2:
-        print "i eh igual a 2";
-        break;
-    default:
-        print "i nao eh igual a 0, 1 ou 2";
-}
 //Resultados:
 /*
     
-i eh igual a um
-    
-i nao eh igual a 0, 1 ou 2
+maca - laranja - pera - banana -
+
  */
