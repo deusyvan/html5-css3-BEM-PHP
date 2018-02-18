b546d4b6bc7e8b265e2b1c8ba32bab74277a1906
deusyvan <deusyvan@gmail.com>
2018-02-18 16:34:25 -0300

    PHP - Manipulação de arquivos e diretorios: fwrite

--- classe/EstudoPHP.php
+++ classe/EstudoPHP.php
@@ -1,37 +1,33 @@
 <?php
 
 //Manipulação de Arquivos e Diretorios
-//feof = retorna true se o ponteiro estiver no fim do arquivo EOF
-//fgets = lê uma linha do arquivo retorna uma string 
+//fwrite: grava no arquivo
+//fclose: fecha o arquivo aberto apontado pelo identificador de arquivo retorna true se sucesso.
 
-$fd = fopen("/etc/fstab", "r");
+//abre o arquivo
+$fd = fopen("/home/deusyvan/Documentos/file.txt", "w");
 
-while (!feof($fd)){
-    // lê uma linha do arquivo
-    $buffer = fgets($fd, 4096);
-    
-    //imprime a linha.
-    echo $buffer;
-}
+//escreve no arquivo
+fwrite($fd, "linha 1\n");
+fwrite($fd, "linha 2\n");
+fwrite($fd, "linha 3\n");
+
+//fecha o arquivo
 fclose($fd);
 
 
 //Resultados:
 /*
     
-# /etc/fstab: static file system information.
-#
-# Use 'blkid' to print the universally unique identifier for a
-# device; this may be used with UUID= as a more robust way to name devices
-# that works even if disks are added and removed. See fstab(5).
-#
-# <file system> <mount point>   <type>  <options>       <dump>  <pass>
-# / was on /dev/sda1 during installation
-UUID=26dc18c0-4bcb-493c-b484-d4774aa9a5b1 /               ext4    errors=remount-ro 0       1
-# swap was on /dev/sda5 during installation
-UUID=7097abc6-0e06-4198-a10f-1406ef149815 none            swap    sw              0       0
-/dev/sr0        /media/cdrom0   udf,iso9660 user,noauto     0       0
+Existindo o arquivo vai apagar ele inteiro e inserir:
+linha 1
+linha 2
+linha 3
 
+Não existindo ele cria e escreve: 
+linha 1
+linha 2
+linha 3
 
  */
 
