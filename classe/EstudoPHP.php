ade23a2cfd558e53299aa30bd295004cd6970571
deusyvan <deusyvan@gmail.com>
2018-02-18 14:14:55 -0300

    PHP - Requisição de arquivos: include e require once

--- classe/EstudoPHP.php
+++ classe/EstudoPHP.php
@@ -1,7 +1,15 @@
 <?php
 
 //Requisições de Arquivos
-//include
+//require diferente de include somente na manipulação de erros.
+// include no caso de erro lança um warnig
+// require no caso de erro lança um erro fatal
+
+/* include_once = igual o include, a não ser que já tenha sido incluído evitando sobreposição
+ * 
+ * require_once = igual o require, a não ser que já tenha sido incluído evitando sobreposição
+ * 
+ * */
 
 include '../lib/Biblioteca.php';
 
